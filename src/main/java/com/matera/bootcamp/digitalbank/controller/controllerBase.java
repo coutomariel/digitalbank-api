14231c17e75d61b461e54594e74e98c86f64d2bf
coutomariel <coutomariel@gmail.com>
2021-02-03 08:03:34 -0300

    Adicionando tratamento de exceptions do bean validation e exceptions genéricas

--- src/main/java/com/matera/bootcamp/digitalbank/controller/controllerBase.java
+++ src/main/java/com/matera/bootcamp/digitalbank/controller/controllerBase.java
@@ -1,5 +1,8 @@
 package com.matera.bootcamp.digitalbank.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.matera.bootcamp.digitalbank.dto.response.ErroResponseDto;
 import com.matera.bootcamp.digitalbank.dto.response.ResponseDto;
 import com.matera.bootcamp.digitalbank.exception.ServiceException;
@@ -7,6 +10,9 @@ import com.matera.bootcamp.digitalbank.exception.ServiceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Controller
@@ -17,4 +23,26 @@ public abstract class controllerBase {
         ErroResponseDto erro = new ErroResponseDto(exception.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseDto.comErro(erro));
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDto<Object>> handleException(MethodArgumentNotValidException exception) {
+        List<ErroResponseDto> erros = new ArrayList<>();
+
+        BindingResult bindResult = exception.getBindingResult();
+
+        for (FieldError fieldError : bindResult.getFieldErrors()) {
+            String campo = fieldError.getField();
+            String mensagem = fieldError.getDefaultMessage();
+            erros.add(new ErroResponseDto(campo, mensagem));
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseDto.comErros(erros));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseDto<Object>> handleException(Exception exception) {
+        ErroResponseDto erro = new ErroResponseDto("Erro inseperado ao processar requisição.");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ResponseDto.comErro(erro));
+    }
+
 }
